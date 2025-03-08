a16a8b2f5df549f32d8ed02f9599ce060bb75e70
GAGAN SHETTY <144126593+ShadowmaxCoder@users.noreply.github.com>
2025-03-08 09:34:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['your-cdn.com'], // Add if using external images
-    },
-}
-
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  }
+};
